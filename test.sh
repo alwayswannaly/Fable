949955efe0e3d61a2e942cacf4c103b72289b58c
Shubham Anand <shubham@interviewbit.com>
2022-04-08 00:48:47 +0530

    change script for model testing

--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
-python3 main.py --type lda $1 --model_name soft_engg
+a=$(aws s3 cp $1 tmp/)
+file_name=$(ls tmp/)
+idx=0
+file_path=tmp/${file_name[$idx]}
+python3 main.py --type lda $file_path --model_name soft_engg && rm $file_path
